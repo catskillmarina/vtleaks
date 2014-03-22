68f65842d533c7482809e5bc650be7dcebb0c666
Marina Brown <catskillmarina@gmail.com>
2014-03-21 22:08:37 -0400

    push to site

--- vtleaks-source/development.sh
+++ vtleaks-source/development.sh
@@ -4,6 +4,6 @@ sed -i -e '/^url: http:\/\/vtleaks.org\//s/^url/#url/'  \
        -e '/^#url: http:\/\/localhost:4000\//s/^#url/url/' \
        _config.yml
 
-jekyl serve --watch
+jekyll serve --watch
 
 
